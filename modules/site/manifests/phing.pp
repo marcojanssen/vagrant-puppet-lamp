e7bedcd13d04ef53a0389f7907ec7c5485d83442
Marco Janssen <marco.janssen@outlook.com>
2013-06-04 20:52:04 +0200

    Fixed phing channel discovery, since it cant be found with auto discovery

--- modules/site/manifests/phing.pp
+++ modules/site/manifests/phing.pp
@@ -1,7 +1,14 @@
 class site::phing {
-    exec { "phing-install":
-        command => "pear install phing/phing",
+
+    exec { "phing-pear-discover":
+        command => "pear channel-discover pear.phing.info",
         unless => "pear info phing/phing",
         require => Exec['pear-auto-discover'],
+    }
+
+    exec { "phing-install":
+        command => "pear install pear.phing.info/phing",
+        unless => "pear info phing/phing",
+        require => Exec['phing-pear-discover'],
     }
 }
