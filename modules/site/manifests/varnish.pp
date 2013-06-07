76611930f5dffc4ab63e25db2b44a0d8ade1a4ba
Marco Janssen <marco.janssen@outlook.com>
2013-06-07 20:00:07 +0200

    Added varnish config to set varnish to port 8080 and the backend at port 80

--- modules/site/manifests/varnish.pp
+++ modules/site/manifests/varnish.pp
@@ -1,6 +1,18 @@
 class site::varnish {
     package { "varnish":
-      ensure   => present,
-      require  => Exec["apt-update"]
+        ensure   => present,
+        require  => Exec["apt-update"]
+    }
+
+    file {
+        "/etc/varnish/default.vcl":
+        ensure => present,
+        require => Package["varnish"],
+        source => "puppet:///modules/site/varnish/default.vcl";
+
+        "/etc/default/varnish":
+        ensure => present,
+        require => Package["varnish"],
+        source => "puppet:///modules/site/varnish/varnish";
     }
 }
