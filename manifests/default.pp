e1250490cd1a9d69e70fe15268c0f3394b8eff15
Marco Janssen <marco.janssen@outlook.com>
2013-08-29 16:21:55 +0200

    Added phpunit

--- manifests/default.pp
+++ manifests/default.pp
@@ -13,6 +13,7 @@ include project
 include project::composer
 include project::web
 include project::php5
+include project::phpunit
 include project::pear
 include project::phing
 include project::sql
